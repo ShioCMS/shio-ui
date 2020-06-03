60ada0fcfa37a384204b7ed14b7158e9721343fa
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-06-03 19:07:48 -0300

    - bootstap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -9,11 +10,14 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   imports: [
+   
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-    ])
+    ]),
+    FormsModule,
+    NgbModule
   ],
   declarations: [
     AppComponent,
